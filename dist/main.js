09bfeb01284db2bbecd8e1640816a54d23932dc9
makanta <mktmakantamkt@gmail.com>
2024-02-13 20:18:09 +0100

    some changes

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,11 @@
 let cancelBtn = document.querySelector('.close');
 let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.hide');
+let viewedImage = document.querySelector('.images-con img');
+let previous = document.querySelector('.previous-arrow');
+let next = document.querySelector('.next-arrow');
+
+let images = ['one', 'two', 'three', 'four']
 
 cancelBtn.addEventListener('click', (e) => {
   nav.style.display ="none";
@@ -9,4 +14,16 @@ hamburger.addEventListener('click', (e) => {
   // nav.classList.add('hide');
   nav.style.display ="block";
 
+})
+
+viewedImage.src = '/images/image-'+present+'.jpg'
+
+let present = images[theOne];
+let theOne = 0;
+
+previous.addEventListener('click',  (e) => {
+  theOne--;
+})
+next.addEventListener('click',  (e) => {
+  theOne++;
 })
