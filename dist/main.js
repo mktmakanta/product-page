35510b2350569547999935f9ff68defd8aa9ab1d
makanta <mktmakantamkt@gmail.com>
2024-02-13 21:30:11 +0100

    commiting before doing tablet view

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,31 @@
+//order amount
+let quantity = document.querySelector('.quantity');
+let minus = document.querySelector('.minus');
+let add = document.querySelector('.plus');
+
+let count = parseInt(quantity.textContent);
+
+
+minus.addEventListener("click", (e) => {
+  if (count === 0){
+      count = 0;
+  } else {
+    count--;
+  }
+  quantity.textContent = count; 
+});
+
+add.addEventListener("click", (e) => {
+  count++;
+  quantity.textContent = count; 
+
+});
+
+
+//navigation
 let cancelBtn = document.querySelector('.close');
 let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.hide');
-let viewedImage = document.querySelector('.images-con img');
-let previous = document.querySelector('.previous-arrow');
-let next = document.querySelector('.next-arrow');
-
-let images = ['one', 'two', 'three', 'four']
 
 cancelBtn.addEventListener('click', (e) => {
   nav.style.display ="none";
@@ -16,14 +36,30 @@ hamburger.addEventListener('click', (e) => {
 
 })
 
-viewedImage.src = '/images/image-'+present+'.jpg'
+//images
+let viewedImage = document.querySelector('.images-con img');
+let previous = document.querySelector('.previous-arrow');
+let next = document.querySelector('.next-arrow');
 
-let present = images[theOne];
+let images = ['one', 'two', 'three', 'four'];
 let theOne = 0;
+let present = images[theOne];
 
-previous.addEventListener('click',  (e) => {
+viewedImage.src = '/images/image-' + present + '.jpg';
+
+previous.addEventListener('click', (e) => {
   theOne--;
-})
-next.addEventListener('click',  (e) => {
+  if (theOne < 0) {
+    theOne = 0;
+  }
+  present = images[theOne];
+  viewedImage.src = '/images/image-' + present + '.jpg';
+});
+next.addEventListener('click', (e) => {
   theOne++;
-})
+  if (theOne === images.length) {
+    theOne = images.length -1;
+  }
+  present = images[theOne];
+  viewedImage.src = '/images/image-' + present + '.jpg';
+});
